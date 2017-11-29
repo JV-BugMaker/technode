075c17ff996115ced4a7ee60167dc8de29aa007c
JV-BugMaker <794241063@qq.com>
2017-11-29 23:54:42 +0800

    [*]FE rooms list logic

--- static/controllers/rooms.js
+++ static/controllers/rooms.js
@@ -1,4 +1,25 @@
 angular.module('technodeApp').controller('RoomsCtrl',function($scope){
     //rooms fe logic
-    
+    socket.emit('getAllRooms');
+    socket.on('roomsData',function(rooms){
+        $scope.rooms = $scope._rooms = rooms;
+    });
+    $scope.searchRoom = function(){
+        if($scope.searchKey){
+            $scope.rooms = $scope._rooms.filter(function(room){
+                return room.name.indexOf($scope.searchKey) > -1;
+            });
+        }else{
+            $scope.rooms = $scope._rooms;
+        }
+    };
+    $scope.createRoom = function(){
+        socket.emit('createRoom',{
+            name:$scope.searchKey
+        });
+    }
+    socket.on('roomAdded',function(room){
+        $scope._rooms.push(room);
+        $scope.searchRoom();
+    });
 });
